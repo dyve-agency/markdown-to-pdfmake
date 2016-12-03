1978366ee0b22c4f313cdc7369fb50f194c0f4b6
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2016-12-03 14:39:50 +0100

    more nesting

--- test/render_spec.js
+++ test/render_spec.js
@@ -1,3 +1,4 @@
+import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import MarkdownIt from 'markdown-it'
 import render from '../src/renderer'
@@ -9,14 +10,14 @@ describe('render', () => {
     const result = render([])
     expect(result).to.eql([])
   })
-  
+
   describe('Inline renderer', () => {
     describe('Emphasis', () => {
       it('should return an array with one text element which is italic', () => {
         const text = '*This is italic text*'
         const tokens = md.parseInline(text, {})
         const result = render(tokens)
-        expect(result).to.eql([{ text: 'This is italic text', italics: true }])
+        expect(result).to.eql([{ text: ['This is italic text'], italics: true }])
       })
     })
 
@@ -25,7 +26,28 @@ describe('render', () => {
         const text = '**This is bold text**'
         const tokens = md.parseInline(text, {})
         const result = render(tokens)
-        expect(result).to.eql([{ text: 'This is bold text', bold: true }])
+        expect(result).to.eql([{ text: ['This is bold text'], bold: true }])
+      })
+    })
+
+    describe('Emphasis and Strong combined', () => {
+      it('should return an array with one text element which is italic', () => {
+        const text = '*This is **italic** text*'
+        const tokens = md.parseInline(text, {})
+        const result = render(tokens)
+        console.log(result)
+        expect(result).to.eql(
+          [
+            {
+              text: [
+                'This is ',
+                { text: 'italic', bold: true },
+                ' text'
+              ],
+              italics: true
+            }
+          ]
+        )
       })
     })
   })
