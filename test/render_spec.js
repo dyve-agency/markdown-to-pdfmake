22cd7125b8b5d9257985be0d7e3e2558de791f83
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2016-12-05 22:12:26 +0100

    tests

--- test/render_spec.js
+++ test/render_spec.js
@@ -17,7 +17,7 @@ describe('render', () => {
         const text = '*This is italic text*'
         const tokens = md.parseInline(text, {})
         const result = render(tokens)
-        expect(result).to.eql([{ text: ['This is italic text'], italics: true }])
+        expect(result).to.eql([{ text: 'This is italic text', italics: true }])
       })
     })
 
@@ -26,7 +26,7 @@ describe('render', () => {
         const text = '**This is bold text**'
         const tokens = md.parseInline(text, {})
         const result = render(tokens)
-        expect(result).to.eql([{ text: ['This is bold text'], bold: true }])
+        expect(result).to.eql([{ text: 'This is bold text', bold: true }])
       })
     })
 
@@ -45,6 +45,29 @@ describe('render', () => {
                 ' text'
               ],
               italics: true
+            }
+          ]
+        )
+      })
+    })
+  })
+  describe('List', () => {
+    describe('Numbered list', () => {
+      it('should return an array with one text element which is italic', () => {
+        let text = '1. Lorem ipsum dolor sit amet\n'
+        text += '2. Consectetur adipiscing elit\n'
+        text += '3. Integer molestie lorem at massa\n'
+
+        const tokens = md.parse(text, {})
+        const result = render(tokens)
+        expect(result).to.eql(
+          [
+            {
+              ol: [
+                'Lorem ipsum dolor sit amet',
+                'Consectetur adipiscing elit',
+                'Integer molestie lorem at massa'
+              ]
             }
           ]
         )
